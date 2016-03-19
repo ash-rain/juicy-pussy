5cec7210e3c1c74b2fa0d5bf99b1d9aa7c93320a
ash <ash@mtr-design.com>
2016-03-19 15:00:30 +0200

    server dirs

--- pussy/server.js
+++ pussy/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use('/node_modules', express.static('node_modules'));
+app.use('/app', express.static('app'));
+app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
